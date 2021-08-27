abdb20ee049ef0cf0f08e1cf063093c2a319d1cf
cyber-talhaaa <talhadar13579@gmail.com>
2021-08-27 11:58:28 +0500

    Firebase Backend Added

--- app.js
+++ app.js
@@ -2,53 +2,92 @@ const inputEl = document.getElementById('todoText');
 const addBtn = document.getElementById('addTodo');
 const deleteAllBtn = document.getElementById('deleteAll');
 const todoListEl = document.getElementById('list');
-let bool = true;
-let editItem;
+let itemId;
+
+//? Firebase Data Request
+const request = (id, obj = {}) =>
+    fetch(`https://todo-29872-default-rtdb.firebaseio.com/${id}.json`, obj);
+
+//? Fetch Todos from FireBase
+const renderTodos = async() => {
+    const response = await request('/todos');
+    const res = await response.json();
+
+    for (const todo in res) {
+        createTodo(res[todo], todo);
+    }
+};
+
+//? Render Todos on Startup
+renderTodos();
 
 //? Single Element Edit Handler
 const editHandler = (e) => {
     inputEl.value = e.target.previousSibling.textContent;
-    bool = false;
     addBtn.textContent = 'Save';
-    editItem = e.target;
+    itemId = e.target;
 };
 
 //? Single Element Delete Handler
 const deleteHandler = (e) => {
-    if (bool) e.target.parentElement.remove();
+    const id = e.target.parentElement.id;
+    if (!itemId) {
+        e.target.parentElement.remove();
+        request(`/todos/${id}`, { method: 'DELETE' });
+    }
+};
+
+//? Create a Todo Item
+const createTodo = (value, id) => {
+    const liEl = document.createElement('li');
+    const editBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+
+    editBtn.addEventListener('click', editHandler);
+    deleteBtn.addEventListener('click', deleteHandler);
+
+    liEl.id = id;
+    editBtn.className = 'edit';
+    editBtn.textContent = 'edit';
+    deleteBtn.className = 'delete';
+    deleteBtn.textContent = 'delete';
+    liEl.textContent = value;
+    liEl.append(editBtn, deleteBtn);
+    todoListEl.appendChild(liEl);
 };
 
 //? Add and Edit Handler Function
 const addElementHandler = (e) => {
     const inputValue = inputEl.value;
 
-    if (inputValue.trim() && bool) {
-        const liEl = document.createElement('li');
-        const editBtn = document.createElement('button');
-        const deleteBtn = document.createElement('button');
-
-        editBtn.addEventListener('click', editHandler);
-        deleteBtn.addEventListener('click', deleteHandler);
-
-        editBtn.className = 'edit';
-        editBtn.textContent = 'edit';
-        deleteBtn.className = 'delete';
-        deleteBtn.textContent = 'delete';
-        liEl.textContent = inputValue;
-        liEl.append(editBtn, deleteBtn);
-        todoListEl.appendChild(liEl);
-
-        inputEl.value = '';
+    if (inputValue.trim() && !itemId) {
+        request('/todos', {
+                method: 'POST',
+                body: JSON.stringify(inputValue),
+                headers: {
+                    'content-type': 'application/json',
+                },
+            })
+            .then((res) => res.json())
+            .then((r) => createTodo(inputValue, r.name));
     }
 
-    if (inputValue.trim() && !bool) {
-        bool = true;
+    if (inputValue.trim() && itemId) {
+        const id = itemId.parentElement.id;
         addBtn.textContent = 'Add Todo';
+        itemId.previousSibling.textContent = inputEl.value;
+        itemId = undefined;
 
-        editItem.previousSibling.textContent = inputEl.value;
-
-        inputEl.value = '';
+        request(`/todos/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(inputValue),
+            headers: {
+                'content-type': 'application/json',
+            },
+        });
     }
+
+    inputEl.value = '';
 };
 
 //? Add New Element
@@ -60,5 +99,8 @@ document.addEventListener('keypress', (e) => {
 
 //? Delete All Elements
 deleteAllBtn.addEventListener('click', (e) => {
-    if (bool) todoListEl.innerHTML = '';
+    if (!itemId) {
+        todoListEl.innerHTML = '';
+        request('/todos', { method: 'DELETE' });
+    }
 });
